0748b6cc03ad0b87eebea0015bf65c8d15c2d8a1
KatSKe <katarina.skenderovic13@gmail.com>
2026-03-11 20:19:34 +0100

    Završetak upoznavanja s React

---
+++ 3_REACT/helloReact/moduli/matematika.js
@@ -0,0 +1,20 @@
+export const PI=3.14 // Iz ove je datoteke moguće napraviti import varjiable PI
+
+export function zbroji(a,b){ 
+    return a+b
+}
+
+export default function pozdrav(ime){
+    return `Hello, ${ime}`
+}
+
+function prva(){
+    return `Ovo vidim jer je napravljen export. ${druga()}`
+}
+
+function druga (){
+    return 'Ovo ne vidim izvan matematika.js'
+
+}
+
+export {prva}
