2d71bde918e3ad8166dbd77d2b09691edd8b9bf9
KatSKe <katarina.skenderovic13@gmail.com>
2026-03-17 20:40:02 +0100

    LaLaBooking-nastavak

---
+++ 3_REACT/EdunovaAPP/src/pages/smjerovi/SmjerPregled.jsx
@@ -0,0 +1,27 @@
+import { useEffect, useState } from "react"
+import SmjerService from "../../services/smjerovi/SmjerService"
+
+export default function SmjerPregled(){
+
+    const [smjerovi, setSmjerovi] = useState([])
+
+    useEffect(()=>{
+        ucitajSmjerove()
+    },[])
+
+    async function ucitajSmjerove() {
+        await SmjerService.get().then((odgovor)=>{
+            setSmjerovi(odgovor.data)
+        })
+    }
+
+    return(
+        <>
+        <ol>
+            {smjerovi && smjerovi.map((smjer)=>(
+                <li>{smjer.naziv} </li>
+            ))}
+        </ol>
+        </>
+    )
+}
