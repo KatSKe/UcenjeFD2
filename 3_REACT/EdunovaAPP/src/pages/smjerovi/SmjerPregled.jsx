e7466de2dc8d884a83ffac974359d16789b4ee95
KatSKe <katarina.skenderovic13@gmail.com>
2026-03-19 19:27:18 +0100

    Formatiranje datuma na appEdunova

--- 3_REACT/EdunovaAPP/src/pages/smjerovi/SmjerPregled.jsx
+++ 3_REACT/EdunovaAPP/src/pages/smjerovi/SmjerPregled.jsx
@@ -3,6 +3,7 @@ import SmjerService from "../../services/smjerovi/SmjerService"
 import { Table } from "react-bootstrap"
 import { NumericFormat } from "react-number-format"
 import { GrValidate } from "react-icons/gr"
+import FormatDatuma from "../../components/FormatDatuma"
 
 export default function SmjerPregled(){
 
@@ -32,34 +33,34 @@ export default function SmjerPregled(){
                 </tr>
             </thead>
             <tbody>
-                {smjerovi &&smjerovi.map((smjer)=>(
-                    <tr>
-                        <td>{smjer.naziv}</td> 
-                        <td>{smjer.trajanje} h</td> 
-                        <td>
+                {smjerovi && smjerovi.map((smjer)=>(
+             <tr key={smjer.sifra}>
+                        <td>{smjer.naziv}</td>
+                        <td className='text-end'>{smjer.trajanje} h</td>
+                        <td className='desno'>
                             <NumericFormat
                             value={smjer.cijena}
                             displayType={'text'}
                             thousandSeparator='.'
                             decimalSeparator=','
-                            suffix='€'     
-                            decimalScale={2}  
+                            suffix=' €'
+                            prefix='='
+                            decimalScale={2}
                             fixedDecimalScale
-                            />   
-                        </td>           
-                        <td>{smjer.datumPokretanja}</td>
-                         <td>
+                            />
+                        </td>
+                        <td>
+                            <FormatDatuma datum={smjer.datumPokretanja} />
+                        </td>
+                        
+                        <td style={{textAlign: 'center'}}>
                             <GrValidate
                             size={25}
                             color={smjer.aktivan ? 'green' : 'red'}
                             />
-                         </td>
-                        <td>{smjer.aktivan}</td>
+                        </td>
                         <td></td>
                     </tr>
-
-                    
-
                 ))}
             </tbody>
         </Table>
